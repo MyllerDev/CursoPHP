3da586cbf551131eacbc93685bfe940dff820720
myllerdev <myllerx3m@gmail.com>
2025-03-07 19:21:06 -0500

    creando carpeta para el curso de desarrollo web II unicordoba

---
+++ desarrolloWeb-II/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// Esto es un comentario 
+echo 'hola mundo <br>'; //echo se utiliza para imprimir en pantalla un texto
+echo 'hola mundo'.'2025 <br>'; //para concatenar se hace con un . y no con +
+echo '<button>Click</button>'; //para imprimir un boton en pantalla
---
+++ desarrolloWeb-II/tiposDatos.php
@@ -0,0 +1,5 @@
+
+
+<?php
+
+// las variables no son tipadas, es decir no se especifica el tipo de dato que se va a almacenar
