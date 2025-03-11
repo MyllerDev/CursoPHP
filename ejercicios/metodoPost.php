1eb809714c3b51a466b0fc14babe07703e1e2c78
myllerdev <myllerx3m@gmail.com>
2025-03-10 21:38:14 -0500

    subiendo nuevos archivos aprendiendo php

---
+++ ejercicios/metodoPost.php
@@ -0,0 +1,24 @@
+
+<?php
+
+if($_POST){
+    $nombre = $_POST['txtNombre'];
+    echo "Tu nombre es: ".$nombre;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HOLA MUNDO</h1>
+    <form action="incluirhtml.php" method="post">
+        <input type="text" name="txtNombre">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
