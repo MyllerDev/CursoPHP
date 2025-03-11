200021f63817b93a2b007bda5ef031cdb2b71ce4
myllerdev <myllerx3m@gmail.com>
2025-03-10 19:48:58 -0500

    eliminando archivo duplicado

--- imprimirDatos.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo 'hola mundo con echo'; //echo es una funcion que imprime en pantalla unicamente cadenas de texto
-print_r(('hola mundo con print')); //print_r es una funcion que imprime en pantalla cualquier tipo de dato
-print('hola mundo con print'); //print es una funcion que imprime en pantalla unicamente cadenas de texto
-
-?>
