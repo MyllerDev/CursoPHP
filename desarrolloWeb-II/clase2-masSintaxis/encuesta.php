994c0ab6eccef66890445d89355cd589f4c9ff0f
myllerdev <myllerx3m@gmail.com>
2025-03-17 21:41:41 -0500

    Agregando la funcionalidad de mostrar los datos en una tabla de la encuenta

--- desarrolloWeb-II/clase2-masSintaxis/encuesta.php
+++ desarrolloWeb-II/clase2-masSintaxis/encuesta.php
@@ -1,65 +1,80 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Encuesta de Estudiantes</title>
+    <link rel="stylesheet" href="styles.css"> 
 </head>
 
 <body>
 
-    
-    <form action="encuesta.php" method="post">
-        <h2>Encuesta</h2>
-        <fieldset style="text-align: center; width: 50%;">
-            <legend >Cantidad de Encuestado</legend>
+    <div class="container">
+        <form action="" method="post">
+            <h2>Encuesta</h2>
+            <fieldset>
+                <legend>Cantidad de Encuestados</legend>
+                <input type="number" name="cant" min="1" placeholder="Digite el número de estudiantes" required>
+                <br>
+                <input type="submit" name="generar" value="Generar Encuesta">
+            </fieldset>
+        </form>
 
-            <input type="number" name="cant" min="1" placeholder="Digite el numero de estudiantes"><br>
-            <input type="submit" name="Enviar">
-        </fieldset>
-    </form>
+        <?php
+        if (isset($_POST['generar'])) {
+            $cant = $_POST['cant'];
+            echo "<form action='' method='post'>";
+            echo "<input type='hidden' name='cant' value='$cant'>"; // Guardar cantidad
+
+            for ($i = 0; $i < $cant; $i++) {
+                echo "
+                <fieldset>
+                    <legend>Estudiante N° " . ($i + 1) . "</legend>
+                    <input type='number' name='ide[]' min='1' placeholder='Identificación' required><br>
+                    <input type='text' name='nombre[]' placeholder='Nombre' required><br>
+
+                    <label>Sexo:</label>
+                    <input type='radio' name='sexo[$i]' value='M' required> M
+                    <input type='radio' name='sexo[$i]' value='F' required> F <br>
+
+                    <label>Fecha de Nacimiento:</label>
+                    <input type='date' name='fechaNacimiento[]' required><br>
+                </fieldset>";
+            }
+
+            echo "<input type='submit' name='guardar' value='Guardar Datos' class='btn'>";
+            echo "</form>";
+        }
+
+        if (isset($_POST['guardar'])) {
+            $cant = $_POST['cant'];
+            $ide = $_POST['ide'];
+            $nombre = $_POST['nombre'];
+            $sexo = $_POST['sexo'] ?? []; // Si no se selecciona, evita errores
+            $fechaNace = $_POST['fechaNacimiento'];
+
+            echo "<h2>Datos Ingresados:</h2>";
+            echo "<table>";
+            echo "<tr><th>ID</th><th>Nombre</th><th>Sexo</th><th>Fecha de Nacimiento</th></tr>";
+
+            for ($i = 0; $i < $cant; $i++) {
+                // Se usa ternario para evitar errores si no existe la clave en el array
+                $sexoValor = isset($sexo[$i]) ? $sexo[$i] : 'No especificado';
+
+                echo "<tr>
+                        <td>{$ide[$i]}</td>
+                        <td>{$nombre[$i]}</td>
+                        <td>{$sexoValor}</td>
+                        <td>{$fechaNace[$i]}</td>
+                      </tr>";
+            }
+
+            echo "</table>";
+        }
+        ?>
+    </div>
 
 </body>
 
 </html>
-
-<?php
-
-if ($_POST) {
-    $cant = $_POST['cant'];
-
-    for ($i = 1; $i <= $cant; $i++) {
-        echo "
-        <form action = 'encuesta.php' method = 'post'>
-        <form action='encuesta.php' method='post'>
-
-        <fieldset style='text-align: center; width: 50%;'>
-            <legend >Estudiante N° .$i</legend>
-
-    <input type='number' name='ide' min='1' placeholder='identificacion'>
-    <input type='text' name='nombre'  placeholder='Digite su nombre'>
-    <input type='radio' name='sexo' value='M'>M
-    <input type='radio' name='sexo' value='F'>F
-    <input type='date' name='fechaNacimiento'  placeholder='fecha Nacimiento'>
-    </fieldset>
-    </form>
-    </form>";
-    
-    }
-
-    echo "<input type = 'Submit' name = 'Enviar'>";
-    
-    /*
-    $ide = $_POST['ide'];
-    $nombre = $_POST['nombre'];
-    $sexo = $_POST['sexo'];
-    $fechaNace = $_POST['fechaNacimiento'];
-    
-    for($j = 1; $j < $cant; $j++){
-        echo $nombre[$i];
-    }*/
-}
-
-
-?>
